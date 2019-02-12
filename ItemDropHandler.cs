a9baf0b03a737b2231bf885caf6f5e70a6aa0850
Lilwu <bboywu@gmail.com>
2019-02-12 11:25:50 +0800

    ItemDropHandler.cs 第一次提交(2019.02.12)

---
+++ ItemDropHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ItemDropHandler : MonoBehaviour, IDropHandler
+{
+    public Inventory inventory;
+    public void OnDrop(PointerEventData eventData)
+    {
+        RectTransform invPanel = transform as RectTransform;
+
+        if (!RectTransformUtility.RectangleContainsScreenPoint(invPanel,
+        Input.mousePosition))
+        {
+            IInventoryItem item = eventData.pointerDrag.gameObject.GetComponent<ItemDragHandler>().Item;
+            if(item != null)
+            {
+                inventory.RemoveItem(item);
+                item.OnDrop();
+                print("丟掉第" + item + "個武器");
+            }
+        }
+    }
+}
