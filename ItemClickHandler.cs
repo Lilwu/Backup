432984819bd2f47547dec8bae3f0bc1133024642
Lilwu <bboywu@gmail.com>
2019-02-12 11:24:39 +0800

    ItemClickHandler.cs 第一次提交(2019.02.12)

---
+++ ItemClickHandler.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemClickHandler : MonoBehaviour
+{
+    public Inventory _inventory;
+
+    public void OnItemClicked()
+    {
+        ItemDragHandler dragHandler =
+            gameObject.transform.Find("ItemImage").GetComponent<ItemDragHandler>();
+
+        IInventoryItem item = dragHandler.Item;
+
+        Debug.Log(item.Name);
+
+        _inventory.UseItem(item);
+
+        item.OnUse();
+    }
+
+}
