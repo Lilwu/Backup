e214a4fc6fc3a546d29d9d6beeace521ab551ffa
Lilwu <bboywu@gmail.com>
2019-02-12 11:28:43 +0800

    PlayerHealth.cs 第一次提交(2019.02.12)

---
+++ PlayerHealth.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public Image playerHealth;
+    public Text healthText;
+    private float maxHealth;
+    private int currentHealth;
+
+    void Start()
+    {
+        maxHealth = GetComponent<Player>().MaxHP;
+        currentHealth = GetComponent<Player>().HP;
+        healthText.text = currentHealth + "/" + maxHealth;
+
+        print("最大血量為：" + maxHealth);
+        print("目前血量為：" + currentHealth);
+    }
+
+    void TakeDamge(int damage)
+    {
+        if(Input.GetKeyDown(KeyCode.H) && currentHealth > 0)
+        {
+            currentHealth -= damage;
+            playerHealth.fillAmount = currentHealth / maxHealth;
+            healthText.text = currentHealth + "/" + maxHealth;
+
+
+            print("你被扣了25滴血");
+            print("目前血量為：" + currentHealth);
+
+            if(currentHealth <= 0)
+            {
+                Death();
+            }
+        }
+    }
+
+    void Death()
+    {
+        healthText.text = 0 + "/" + maxHealth;
+        print("你已經死亡！");
+        print("目前血量為：" + currentHealth);
+    }
+
+    void Update()
+    {
+        TakeDamge(150);
+    }
+}
