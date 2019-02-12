44635c1b215b4a008d9b001d86e6cc59db648753
Lilwu <bboywu@gmail.com>
2019-02-12 11:26:19 +0800

    LobbyManager.cs 第一次提交(2019.02.12)

---
+++ LobbyManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LobbyManager : MonoBehaviour
+{
+    public TextMesh playerName;
+
+    public void EnterPlayerName(Text enterName)
+    {
+        playerName.text = enterName.text;
+    }
+
+    public void ToGame()
+    {
+        SceneManager.LoadScene("Town");
+
+        print("恭喜創建角色！");
+        print("您的角色名稱為");
+    }
+}
