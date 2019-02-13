4e622b0610f3a34cb429b755ef4f9db856ec9775
Lilwu <bboywu@gmail.com>
2019-02-13 20:42:41 +0800

    InventoryItemBase.cs 新增void UnUsed()(2019.02.13)

--- InventoryItemBase.cs
+++ InventoryItemBase.cs
@@ -2,9 +2,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class InventoryItemBase : MonoBehaviour, IInventoryItem
 {
+    public GameObject inventory;
+
     public virtual string Name
     {
         get
@@ -21,10 +24,15 @@ public class InventoryItemBase : MonoBehaviour, IInventoryItem
         gameObject.SetActive(false);
     }       public virtual void OnDrop()     {
         //TODO: move a logic like this to a base class or helper method
-        RaycastHit hit = new RaycastHit();         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);         if (Physics.Raycast(ray, out hit, 1000))         {             gameObject.SetActive(true);             gameObject.transform.position = hit.point;         }
+        RaycastHit hit = new RaycastHit();         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);         if (Physics.Raycast(ray, out hit, 1000))         {             gameObject.SetActive(true);             gameObject.transform.position = hit.point; //將物品移到丟棄物品的點上 TODO:各場景需有一個回收GameObject;         }
     }
 
     public virtual void OnUse()
+    {
+
+    }
+
+    public virtual void UnUsed()
     {
 
     }
